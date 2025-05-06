f8a31f430f8a970004466bfcd94703a0aa53bfa7
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-06 18:52:41 +0500

    Tech Ever Solutions

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar/Navbar";
+import React from "react";
+
+const Booking = () => {
+  return (
+    <div>
+      Booking
+      <Navbar />
+    </div>
+  );
+};
+
+export default Booking;
