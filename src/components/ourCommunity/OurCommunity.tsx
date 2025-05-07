419faeb28cb62b200dd46a541fb5e18a12ed1de2
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-07 16:47:02 +0500

    working

--- src/components/ourCommunity/OurCommunity.tsx
+++ src/components/ourCommunity/OurCommunity.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import OurCommunityCard from "./OurCommunityCard";
+import MembersIcon from "../../../public/assets/community/members.svg";
+import AssociationsIcon from "../../../public/assets/community/associations.svg";
+import ClubsIcon from "../../../public/assets/community/clubs.svg";
 
 const OurCommunity = () => {
   return (
     <div>
       <div>
-        <h1>Manage your entire community in a single system</h1>
-        <p>Who is Nextcent suitable for?</p>
+        <h1 className=" text-[#4D4D4D] text-[36px] font-semibold leading-[44px] ">
+          Manage your entire community in a single system
+        </h1>
+        <p className=" leading-[24px] text-[#717171] ">
+          Who is Nextcent suitable for?
+        </p>
       </div>
-      <div>
-        <OurCommunityCard />
-        <OurCommunityCard />
-        <OurCommunityCard />
+      <div className=" flex  ">
+        <OurCommunityCard
+          paragraph="Our membership management software provides full automation of membership renewals and payments"
+          heading="Membership Organisations"
+          img={MembersIcon}
+        />
+        <OurCommunityCard
+          paragraph="Our membership management software provides full automation of membership renewals and payments"
+          heading="National Associations"
+          img={AssociationsIcon}
+        />
+        <OurCommunityCard
+          paragraph="Our membership management software provides full automation of membership renewals and payments"
+          heading="Clubs And Groups"
+          img={ClubsIcon}
+        />
       </div>
     </div>
   );
--- src/components/ourCommunity/OurCommunityCard.tsx
+++ src/components/ourCommunity/OurCommunityCard.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Image, { StaticImageData } from "next/image";
 
-const OurCommunityCard = () => {
-  return <div></div>;
+interface OurCommunityCardPropsTypes {
+  img?: string | StaticImageData;
+  heading?: string;
+  paragraph?: string;
+}
+
+const OurCommunityCard: React.FC<OurCommunityCardPropsTypes> = ({
+  img,
+  heading,
+  paragraph,
+}) => {
+  return (
+    <div className="border flex flex-col items-center justify-center py-6 px-4 max-w-[300px] ">
+      <Image src={img!} alt="icon does not show" />
+      <h1 className=" text-[28px] font-bold leading-[36px] text-[#4D4D4D] text-center ">
+        {heading}
+      </h1>
+      <p className=" text-[14px] text-[#717171] leading-[20px] text-center ">
+        {paragraph}
+      </p>
+    </div>
+  );
 };
 
 export default OurCommunityCard;
