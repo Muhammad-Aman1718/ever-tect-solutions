bc2976a044e8e759f2cd35d79b2bf611e8cc4a3e
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-07 14:17:48 +0500

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import HeroSection from "@/components/heroSection/HeroSection";
 import MainContainer from "@/components/MainContainer";
 import Navbar from "@/components/navbar/Navbar";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className=" mx-auto ">
-      <MainContainer className=" border ">
-        <Navbar />
-      </MainContainer>
+    <div className="h-[4000px] ">
+      <div className=" bg-[#F5F7FA] ">
+        <MainContainer className=" border ">
+          <Navbar />
+          <HeroSection />
+        </MainContainer>
+      </div>
     </div>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=" border  flex items-center justify-around  ">
-      <div className="border flex-[1] ">
+    <div className=" border  flex items-center justify-around py-[22px] ">
+      <div className="border flex-[1.5] ">
         <h1>IMG</h1>
       </div>
       <div className="border flex-[3]  ">
         <ul className=" flex items-center justify-around ">
-          <li className="leading-6 text-[#18191F] text-[16px] font-medium ">
+          <li className="leading-6 text-[#18191F] text-[18px] font-medium ">
             Home
           </li>
-          <li className="leading-6 text-[#18191F] text-[16px] font-medium ">
+          <li className="leading-6 text-[#18191F] text-[18px] font-medium ">
             Courses
           </li>
-          <li className="leading-6 text-[#18191F] text-[16px] font-medium ">
+          <li className="leading-6 text-[#18191F] text-[18px] font-medium ">
             Services
           </li>
-          <li className="leading-6 text-[#18191F] text-[16px] font-medium ">
+          <li className="leading-6 text-[#18191F] text-[18px] font-medium ">
             About Us
           </li>
-          <li className="leading-6 text-[#18191F] text-[16px] font-medium ">
+          <li className="leading-6 text-[#18191F] text-[18px] font-medium ">
             Contact Us
           </li>
         </ul>
       </div>
-      <div className=" border flex items-center justify-center flex-[1]  ">
-        <button className=" py-[10px] px-[20px] bg-[#4CAF4F] rounded-[8px] text-white text-[14px] ">
+      <div className=" border flex items-center justify-center flex-[1.5]  ">
+        <button className=" py-[10px] px-[40px] bg-[#4CAF4F] rounded-[4px] text-white text-[14px] ">
           Login
         </button>
       </div>
