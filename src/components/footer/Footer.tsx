65a6d1bcda418595315344feed05a67c5b0fe5e7
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-08 16:47:31 +0500

    working

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,22 +5,25 @@ import Logo from "../../../public/assets/logo.png";
 const Footer = () => {
   return (
     <div className="bg-[#E1F0FF] ">
-      <div>
+      <div className="">
         <div className="flex items-center  ">
           <Image
             src={Logo}
             alt="logo does not show"
-            className="w-[150px] h-[150px] "
+            className="w-[120px] h-[120px] "
           />
           <div>
-            <h3 className=" text-[#2A75BB] text-[40px] leading-[20px] ">
+            <h3 className=" text-[#2A75BB] text-[30px] leading-[10px] ">
               EVERTECH
             </h3>
-            <h3 className=" text-[38px] leading-[50px] ">SOLUTIONS</h3>
-            <p className=" text-[10px] font-bold leading-0 ">
-              Institute of Skill Development & Software House{" "}
+            <h3 className=" text-[28px] leading-[45px] ">SOLUTIONS</h3>
+            <p className=" text-[8px] font-bold leading-0 ">
+              Institute of Skill Development & Software House
             </p>
           </div>
+        </div>
+        <div>
+          <h1 className=" text-[40px] ">Office Address</h1>
         </div>
       </div>
       <div></div>
